cff78326bded223af5d91f7012040ad252e3b3be
kalodaniel <72191543+kalodaniel@users.noreply.github.com>
2021-10-26 19:41:34 +0200

    read and write to xml

---
+++ BBKHXF_10.26/DomReadBBKHXF/src/dombbkhxf1026/DomWriteBBKHXF01.java
@@ -0,0 +1,60 @@
+package dombbkhxf1026;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+public class DomWriteBBKHXF01 {
+
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		try {
+			File xmlFile = new File("users1BBKHXF.xml");
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+			Document document = documentBuilder.parse(xmlFile);
+			Element documentElement = document.getDocumentElement();
+			
+			Element textNode = document.createElement("firstname");
+			textNode.setTextContent("Kiss");
+			
+			Element textNode1 = document.createElement("lastname");
+			textNode1.setTextContent("Andras");
+			
+			Element textNode2 = document.createElement("profession");
+			textNode2.setTextContent("Programmer");
+			
+			Element nodeElement = document.createElement("user");
+			nodeElement.setAttribute("id", "4");
+			
+			nodeElement.appendChild(textNode);
+			nodeElement.appendChild(textNode1);
+			documentElement.appendChild(nodeElement);
+			document.replaceChild(documentElement, documentElement);
+			
+			Transformer tFormer = TransformerFactory.newInstance().newTransformer();
+			tFormer.setOutputProperty(OutputKeys.METHOD, "xml");
+			
+			Source source = new DOMSource(document);
+			Result result = new StreamResult(xmlFile);
+			tFormer.transform(source, result);
+
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+	}
+}
